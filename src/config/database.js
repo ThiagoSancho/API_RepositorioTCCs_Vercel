c96890fb7517fed03720ecea96cdcd4141cb2085
LoudAsh21 <thiagosancho2006@gmail.com>
2025-01-23 11:32:20 -0300

    verificar se ja possui conexao com o banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -2,6 +2,12 @@ const mongoose = require("mongoose");
 
 async function connect(string_connection) {
   try {
+    
+    if (mongoose.connection.readyState == 1) {
+      console.log("Já conectado ao banco de dados!");
+      return true;
+    }
+
     console.log("Conectando ao banco de dados...");
     await mongoose.connect(string_connection);
     console.log("Conectado ao banco de dados!");
